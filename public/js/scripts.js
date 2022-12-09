e0957f83c36e57e098ce3c174869f5914385e7d7
Gurvinder98 <43774079+Gurvinder98@users.noreply.github.com>
2022-12-09 18:09:11 +1100

    Practical-4

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,34 +1,36 @@
-const cardList = [
-    {
-      title: "CHAMPIONS LEAGUE",
-      image: "images/sports2.jpg",
-      link: "https://en.wikipedia.org/wiki/UEFA_Champions_League",
-      desciption: "The UEFA Champions League (abbreviated as UCL, or sometimes, UEFA CL) is an annual club football competition organised by the Union of European Football Associations (UEFA)",
-    },
-    {
-      title: "EUROPA LEAGUE",
-      image: "images/sports3.png",
-      link: "https://en.wikipedia.org/wiki/UEFA_Europa_League",
-      desciption: "The UEFA Europa League (abbreviated as UEL, or sometimes, UEFA EL), formerly the UEFA Cup, is an annual football club competition organised since 1971 by the Union of European Football Associations (UEFA)",
-    },
-  ];
+
+
 
   const submitForm = () => {
     let formData = {};
-    formData.first_name = $("#first_name").val();
-    formData.last_name = $("#last_name").val();
-    formData.password = $("#password").val();
-    formData.email = $("#email").val();
+    formData.title = $("#title").val();
+    formData.image = $("#image").val();
+    formData.link = $("#link").val();
+    formData.description = $("#description").val();
   
     console.log("Form Data Submitted: ", formData);
+    addProjectToApp(formData);
   };
+  
+  //ajax function
+  const addProjectToApp = (project) => {
+    $.ajax({
+      url: "/api/projects",
+      data: project,
+      type: "POST",
+      success: (result) => {
+        alert(result.message);
+        location.reload();
+      },
+    })
+  }
   
   const addCards = (items) => {
     items.forEach((item) => {
       let itemToAppend = '<div class="col s4 center-align"><div class="card blue-grey darken-1 medium"><div class="card-image wave-effects waves-block waves-light">'+
       '<img class="activator" src="'+item.image+'" /></div>' +
       '<div class="card-content white-text"><span class="card-title">' +
-      item.title + '</span><p>' + item.desciption + '</p></div>' + 
+      item.title + '</span><p>' + item.description + '</p></div>' + 
       '<div class="card-action"><a target="_blank" href="' + item.link + '">' + item.title +
       '</a></div><div class="card-reveal"><span class="card-title grey-text text-darken-4">' + 
       'HELLO<i class="material-icons right">close</i></span><p class="card-text" style="color: black">Thank YOU</p>' + 
@@ -36,16 +38,26 @@ const cardList = [
       $("#card-section").append(itemToAppend);
     });
   };
+  
+const getProjects = () => {
+  $.get("/api/projects", (response) => {
+    if (response.statusCode == 200) {
+      addCards(response.data);
+    }
+  });
+};
+  
   document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.carousel');
   });
   
-  $(document).ready(function () {
-    $(".materialboxed").materialbox();
-    $("#formSubmit").click(() => {
-      submitForm();
-    });
-    addCards(cardList);
-    $(".modal").modal();
-    $('.carousel').carousel();
+$(document).ready(function () {
+  $(".materialboxed").materialbox();
+  $("#formSubmit").click(() => {
+    submitForm();
   });
+  getProjects();
+  
+  $(".modal").modal();
+  $('.carousel').carousel();
+});
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 // loading express
 var express=require("express")
+var cors=require('cors')
+let projectCollection;
 
 var app= express()
 
@@ -7,11 +9,92 @@ var app= express()
 app.use(express.static(__dirname+'/public'))
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+app.use(cors())
+
+//mongodb connection..
+const MongoClient = require('mongodb').MongoClient;
+const uri='mongodb+srv://gurvinder:sid@cluster0.jl7rzbc.mongodb.net/?retryWrites=true&w=majority'
+const client = new MongoClient(uri, {useNewUrlParser: true})
+
+
+
+
+const createCollection =(collectionName) => {
+    client.connect((err,db) => {
+        projectCollection = client.db().collection(collectionName);
+        if(!err) {
+            console.log('MongoDb Connected Successfully')
+        }
+        else{
+            console.log('DB ERROR:',err);
+            process.exit(1);
+        }
+    })
+    }
+
+
+//post api..
+app.post("/api/projects", (req, res) => {
+    console.log("New project added", req.body);
+    var newProject = req.body;
+    insertProjects(newProject, (err, result) => {
+      if (err) {
+        res.json({
+          statusCode: 400,
+          message: err,
+        });
+      } else {
+        res.json({statusCode: 200, message: "project added successfully", data: result})
+      }})})
+
+
+    //const cardList = [
+    //     {
+    //       title: "CHAMPIONS LEAGUE",
+    //       image: "images/sports2.jpg",
+    //       link: "https://en.wikipedia.org/wiki/UEFA_Champions_League",
+    //       desciption: "The UEFA Champions League (abbreviated as UCL, or sometimes, UEFA CL) is an annual club football competition organised by the Union of European Football Associations (UEFA)",
+    //     },
+    //     {
+    //       title: "EUROPA LEAGUE",
+    //       image: "images/sports3.png",
+    //       link: "https://en.wikipedia.org/wiki/UEFA_Europa_League",
+    //       desciption: "The UEFA Europa League (abbreviated as UEL, or sometimes, UEFA EL), formerly the UEFA Cup, is an annual football club competition organised since 1971 by the Union of European Football Associations (UEFA)",
+    //     },
+    //   ];
 
 // setting portbinding
 var port = process.env.port || 3000;
 
+// get project..
+
+const getProjects = (callback) => {
+    projectCollection.find({}).toArray(callback);
+
+}
+const insertProjects = (project,callback) => {
+    projectCollection.insert(project,callback);
+}
+
+app.get("/api/projects", (req, res) => {
+    getProjects((err, result) => {
+      if (err) {
+        res.json({
+          statusCode: 400,
+          message: err,
+        });
+      } else {
+        res.json({
+          statusCode: 200,
+          message: "Success",
+          data: result,
+        })
+      }
+    })
+  })
+
 app.listen(port,()=>{
     console.log("App listening to:http://localhost:"+port)
+    createCollection('Football')
 
 })
